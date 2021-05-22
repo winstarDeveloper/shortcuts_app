12374cb01e6bd4ce238d71cd5ee1274c441d828a
winstarDeveloper <prayushkale@gmail.com>
2021-05-22 09:31:54 +0530

    changed layout

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// using strict mode
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
